7753b9317d843c46b51306cee74b7ff2b86de86b
RexAdis <66114971+RexAdis@users.noreply.github.com>
2024-01-07 20:24:15 +0000

    1.1.3

--- .ccls-cache/@home@runner@Study-Base-Timer/To-do.c
+++ .ccls-cache/@home@runner@Study-Base-Timer/To-do.c
@@ -10,16 +10,16 @@ typedef struct // Własna struktura danych //
 
 Task *tasks = NULL; // NULL wskazuje na pusty obszar pamięci // 
 
-int lenght = 0; // Zmienna przechowujęca długość listy //
+int length = 0; // Zmienna przechowujęca długość listy //
 
 void addTask(const char *task){   // const- wartość nie może zostać zmieniona //
-   tasks = (task *) realloc(tasks, (lenght + 1) * sizeof(Task)); 
-   tasks[lenght].task = (char *)malloc(strlen(task) +1); 
-   tasks[lenght].completed = 0; 
+   tasks = (task *) realloc(tasks, (length + 1) * sizeof(Task)); 
+   tasks[length].task = (char *)malloc(strlen(task) +1); 
+   tasks[length].completed = 0; 
 
-   strcpy(tasks[lenght].task, task);
+   strcpy(tasks[length].task, task);
 
-   lenght++;
+   length++;
    printf("Dodaj zadanie");
 
   // Relokazlizacja pamięci dostosowanie rozmiaru dynamicznie alokowanej pamięci w trakcie działania programu. Umożliwia dodawać nowe elementy do struktury danych w trakcie działania programu.) //
@@ -38,14 +38,14 @@ void listTasks(){
         status = 'n';
       
     }
-        print("%d. %s [%c]\n", i + 1, tasks[i].taks, status);
+        printf("%d. %s [%c]\n", i + 1, tasks[i].task, status);
     
   }  
     
 }
 
 void markCompleted(int index){
-  if (index <= lenght && index > 0){
+  if (index <= length && index > 0){
     tasks [index - 1].completed = 1;
     printf*("Zadanie ukończone\n"); 
   
@@ -58,7 +58,7 @@ void markCompleted(int index){
 } // Funkcja zaznaczająca zadanie jako zakończone //
 
 void deleteTask(int index){
-  if (index <= lenght && index > 0 ){
+  if (index <= length && index > 0 ){
     index = index - 1;  //index - 1 bo indeksy w tablicy zaczynają się od 0 //
 
 free(tasks[index].task);  // Zwałanie funkcji free() do zwolnienia pamięci//
@@ -68,9 +68,9 @@ for (int i = index; i < length - 1; i++){)
   
 }
 
-    length--; // lenght - 1 bo zmienna przechowująca długość listy //
+    length--; // length - 1 bo zmienna przechowująca długość listy //
 
-    tasks = (task *) realloc(tasks, (lenght * sizeof(Task))); 
+    tasks = (task *) realloc(tasks, (length * sizeof(Task))); 
     
   } else {
 
@@ -84,13 +84,17 @@ for (int i = index; i < length - 1; i++){)
 
 void editTask(int index, const char* task){
 
-  if (index <= lenght && index > 0){
+  if (index <= length && index > 0){
     index + index - 1;
     
-    char *editTask = (char *)realloc(tasks[index].task) strlen(task) + 1);
+    char *editedTask = (char *)realloc(tasks[index].task) strlen(task) + 1);
 
-    if (editTask != NUll){ // != NUll sprawdza czy wskaźnik nie jest równy NULL //
-      tasks[index].task = editTask;
+    if (editedTask != NUll){ // != NUll sprawdza czy wskaźnik nie jest równy NULL //
+      tasks[index].task = editedTask;
+      strcpy(task[length].task, task);
+      
+      // stwórz nową tablicę znaków o długości task + 1 i skopiuj //
+       printf("Zmiana zadania powiodła się\n");
       
     } else 
       printf("Nie udało się zmodyfikować zadania\n");
@@ -107,7 +111,52 @@ void editTask(int index, const char* task){
 
 
 int main(){
+    int choice;
+    int running = 1;
   
+  printf("\nOpcje\n");
+  printf("1. Dodaj zadanie\n");
+  printf("2. Wyświetl zadania\n");
+  printf("3. Wykonane zadania"\n");
+  printf("4. Edytuj zadanie\n");
+  printf("5. Usuń zadanie\n");
+  printf("6. Exit\n");
+
+  while (running){  
+    printf("Wprowadź wybór: 1, 2, 3, 4, 5, 6: ");
+    scanf("%d", &choice); // scanf - funkcja do odczytu danych z konsoli //
+  
+    switch(choice){
+        case 1:
+      printf("Wpisz zadanie: ");
+      getchar();  // Getchar wczytuje jeden znak z konsoli //
+      fgets(taskInput, sizeof(taskInout), stdin); 
+      // Fgets wczytuje wiersz z konsoli //
+      taskInput[strcspn(taskInput, "\n")] = '\0'; // strcspn - funkcja do znalezienia indeksu
+
+      break;
+
+      
+        case 2:
+      
+        case 3:
+      
+        case 4:
+      
+        case 5:
+      
+        case 6:
+
+
+        default:
+        printf("Nipoprawny wybór"\n);
+        break;
+      
+      
+    }
+  }
+  
+  return 0;  
 }
 
 
--- To-do.c
+++ To-do.c
@@ -10,16 +10,16 @@ typedef struct // Własna struktura danych //
 
 Task *tasks = NULL; // NULL wskazuje na pusty obszar pamięci // 
 
-int lenght = 0; // Zmienna przechowujęca długość listy //
+int length = 0; // Zmienna przechowujęca długość listy //
 
 void addTask(const char *task){   // const- wartość nie może zostać zmieniona //
-   tasks = (task *) realloc(tasks, (lenght + 1) * sizeof(Task)); 
-   tasks[lenght].task = (char *)malloc(strlen(task) +1); 
-   tasks[lenght].completed = 0; 
+   tasks = (task *) realloc(tasks, (length + 1) * sizeof(Task)); 
+   tasks[length].task = (char *)malloc(strlen(task) +1); 
+   tasks[length].completed = 0; 
 
-   strcpy(tasks[lenght].task, task);
+   strcpy(tasks[length].task, task);
 
-   lenght++;
+   length++;
    printf("Dodaj zadanie");
 
   // Relokazlizacja pamięci dostosowanie rozmiaru dynamicznie alokowanej pamięci w trakcie działania programu. Umożliwia dodawać nowe elementy do struktury danych w trakcie działania programu.) //
@@ -38,16 +38,16 @@ void listTasks(){
         status = 'n';
       
     }
-        print("%d. %s [%c]\n", i + 1, tasks[i].taks, status);
+        printf("%d. %s [%c]\n", i + 1, tasks[i].task, status);
     
   }  
     
 }
 
 void markCompleted(int index){
-  if (index <= lenght && index > 0){
+  if (index <= length && index > 0){
     tasks [index - 1].completed = 1;
-    printf*("Zadanie ukończone\n"); 
+    printf("Zadanie ukończone\n"); 
   
   } else{
     
@@ -58,19 +58,19 @@ void markCompleted(int index){
 } // Funkcja zaznaczająca zadanie jako zakończone //
 
 void deleteTask(int index){
-  if (index <= lenght && index > 0 ){
+  if (index <= length && index > 0 ){
     index = index - 1;  //index - 1 bo indeksy w tablicy zaczynają się od 0 //
 
 free(tasks[index].task);  // Zwałanie funkcji free() do zwolnienia pamięci//
 
-for (int i = index; i < length - 1; i++){)
+for (int i = index; i < length - 1; i++){
   tasks[i] = tasks[i + 1];
   
 }
 
-    length--; // lenght - 1 bo zmienna przechowująca długość listy //
+    length--; // length - 1 bo zmienna przechowująca długość listy //
 
-    tasks = (task *) realloc(tasks, (lenght * sizeof(Task))); 
+    tasks = (Task *) realloc(tasks, (length * sizeof(Task))); 
     
   } else {
 
@@ -84,13 +84,17 @@ for (int i = index; i < length - 1; i++){)
 
 void editTask(int index, const char* task){
 
-  if (index <= lenght && index > 0){
-    index + index - 1;
+  if (index <= length && index > 0){
+    index = index - 1;
     
-    char *editTask = (char *)realloc(tasks[index].task) strlen(task) + 1);
+    char *editedTask = (char *)realloc(tasks[index].task) strlen(task) + 1);
 
-    if (editTask != NUll){ // != NUll sprawdza czy wskaźnik nie jest równy NULL //
-      tasks[index].task = editTask;
+    if (editedTask != NUll){ // != NUll sprawdza czy wskaźnik nie jest równy NULL //
+      tasks[index].task = editedTask;
+      strcpy(task[length].task, task);
+      
+      // stwórz nową tablicę znaków o długości task + 1 i skopiuj //
+       printf("Zmiana zadania powiodła się\n");
       
     } else 
       printf("Nie udało się zmodyfikować zadania\n");
@@ -107,7 +111,80 @@ void editTask(int index, const char* task){
 
 
 int main(){
+    
+    int choice;
+    int indexInput;
+    int running = 1;
   
+  printf("\nOpcje\n");
+  printf("1. Dodaj zadanie\n");
+  printf("2. Wyświetl zadania\n");
+  printf("3. Wykonane zadania"\n");
+  printf("4. Edytuj zadanie\n");
+  printf("5. Usuń zadanie\n");
+  printf("6. Exit\n");
+
+  while (running){  
+    printf("Wprowadź wybór: 1, 2, 3, 4, 5, 6: ");
+    scanf("%d", &choice); // scanf - funkcja do odczytu danych z konsoli //
+  
+    switch(choice){
+        case 1:
+      printf("Wpisz zadanie: ");
+      getchar();  // Getchar wczytuje jeden znak z konsoli //
+      fgets(taskInput, sizeof(taskInout), stdin); 
+      // Fgets wczytuje wiersz z konsoli //
+      taskInput[strcspn(taskInput, "\n")] = '\0'; // strcspn - funkcja do znalezienia indeksu
+
+      break;
+
+      
+        case 2:
+      listTasks();
+      break;
+        case 3:
+      printf("Wpisz indeks zadania: ");
+      scanf("%d", &indexInput);
+      markCompleted(indesInput);
+
+      break;
+        
+        case 4:
+    
+      printf("Wpisz indeks zadania: ");
+      scanf("%d", &indexInput);
+      printf("Wpisz zadanie do edycji");
+      getrchar();
+      fgets(taskInput, sizeof(taskInout), stdin); 
+      taskInput[strcspn(taskInput, "\n")] = '\0';
+
+      editTask(indexInput, taskInput);
+      break;
+        case 5:
+          
+      printf("Wpisz indeks zadania: ");
+      scanf("%d", &indexInput);
+
+      deleteTask(indexInput);
+
+      break;
+      
+        case 6:
+          running = 0; // running = 0 zakończenie programu //
+          break;
+        
+        default:
+        printf("Nipoprawny wybór"\n);
+        break;
+      
+      
+    }
+  }
+
+  free(tasks); // Zwolnienie pamięci //
+  
+  
+  return 0;  
 }
 
 
